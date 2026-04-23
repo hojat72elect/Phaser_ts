5c33b5e928759f9c8b027cf067caa128cf677501
hojat72elect <hojat72elect@gmail.com>
2026-04-23 14:18:56 -0400

    Added a simple migration of circle. #1

---
+++ phaser_ts/src/geom/Circle.ts
@@ -0,0 +1,153 @@
+/**
+ * A Circle object.
+ *
+ * This is a geometry object, containing numerical values and related methods to inspect and modify them.
+ * It is not a Game Object, in that you cannot add it to the display list, and it has no texture.
+ * To render a Circle you should look at the capabilities of the Graphics class.
+ */
+export class Circle {
+
+    // The x position of the center of the circle.
+    public x: number;
+
+    // The y position of the center of the circle.
+    public y: number;
+
+    // The internal radius of the circle.
+    private _radius: number;
+
+    // The internal diameter of the circle.
+    private _diameter: number;
+
+    /**
+     * @param x The x position of the center of the circle.
+     * @param y The y position of the center of the circle.
+     * @param radius The internal radius of the circle.
+     */
+    constructor(x: number = 0, y: number = 0, radius: number = 0) {
+        this.x = x;
+        this.y = y;
+        this._radius = radius;
+        this._diameter = 2 * radius;
+    }
+
+    /**
+     * Check to see if this circle contains the given (x, y).
+     */
+    public contains(x: number, y: number) {
+        return false; // todo : to be implemented later
+    }
+
+    /**
+     * Update the x, y, radius, and diameter of this circle.
+     */
+    public setTo(x: number = 0, y: number = 0, radius: number = 0) {
+        this.x = x;
+        this.y = y;
+        this._radius = radius;
+        this._diameter = radius * 2;
+
+        return this;
+    }
+
+    /**
+     * The radius of this circle will be zero.
+     * maybe at this point, we could consider this circle as a single point on 2D screen?
+     */
+    public setEmpty() {
+        this._radius = 0;
+        this._diameter = 0;
+
+        return this;
+    }
+
+    /**
+     * Change the position of this circle.
+     */
+    public setPosition(x: number = 0, y?: number) {
+        if (y === undefined) y = x;
+
+        this.x = x;
+        this.y = y;
+
+        return this;
+    }
+
+    public isEmpty() {
+        return (this._radius <= 0);
+    }
+
+    public getRadius() {
+        return this._radius;
+    }
+
+    public setRadius(value: number) {
+        this._radius = value;
+        this._diameter = 2 * value;
+    }
+
+    public getDiameter() {
+        return this._diameter;
+    }
+
+    public setDiameter(value: number) {
+        this._diameter = value;
+        this._radius = value / 2;
+    }
+
+    /**
+     * @returns the x of the leftmost point of the circle.
+     */
+    public getLeft() {
+        return this.x - this._radius;
+    }
+
+    /**
+     * We just move our circle in such a way that its leftmost point's x will be the provided value.
+     */
+    public setLeft(value: number) {
+        this.x = value + this._radius;
+    }
+
+    /**
+     * @returns the x of the rightmost point of the circle.
+     */
+    public getRight() {
+        return this.x + this._radius;
+    }
+
+    /**
+     * We just move our circle in such a way that its rightmost point's x will be the provided value.
+     */
+    public setRight(value: number) {
+        this.x = value - this._radius;
+    }
+
+    /**
+     * @returns the y of the topmost point of the circle.
+     */
+    public getTop() {
+        return this.y - this._radius;
+    }
+
+    /**
+     * We just move our circle in such a way that its topmost point's y will be the provided value.
+     */
+    public setTop(value: number) {
+        this.y = value + this._radius
+    }
+
+    /**
+     * @returns the y of the downmost point of the circle.
+     */
+    public getBottom() {
+        return this.y + this._radius;
+    }
+
+    /**
+     * We just move our circle in such a way that its downmost point's y will be the provided value.
+     */
+    public setBottom(value: number) {
+        this.y = value - this._radius;
+    }
+}
